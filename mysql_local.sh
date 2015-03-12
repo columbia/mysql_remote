54b6b501ceef0c3c287ae28ce50c69537903093b
Jintack <jintack@cs.columbia.edu>
2015-03-11 20:38:29 -0400

    Add transaction per sec

--- mysql_local.sh
+++ mysql_local.sh
@@ -5,6 +5,7 @@ RAW_OUTPUT=tmp/raw.log
 
 TOTAL=tmp/total_time.txt
 AVG=tmp/avg_time.txt
+TRAN=tmp/transaction.txt
 
 function init()
 {
@@ -33,8 +34,10 @@ function mysql_local_test()
 {
 	rm -f TOTAL
 	rm -f AVG
+	rm -f TRAN
 	touch TOTAL
 	touch AVG
+	touch TRAN
 
 	num_threads=$1	
 		
@@ -42,14 +45,18 @@ function mysql_local_test()
 		sysbench --test=oltp --mysql-password=kvm --oltp-table-size=$TABLE_SIZE --num-threads=$num_threads run | tee  $RAW_OUTPUT 
 		grep 'total time:' $RAW_OUTPUT | awk '{ print $3 }' | sed 's/s//' >> TOTAL
 		grep 'avg:' $RAW_OUTPUT | awk '{ print $2 }' | sed 's/ms//' >> AVG
+		grep 'transactions:' $RAW_OUTPUT | awk '{ print $3 }' | sed 's/(//' >> TRAN
 	done;
 
 	# Output in nice format as well
 	echo -en "MySQL (localhost) \t$num_threads threads\ttotal time\t" >> $OUTFILE
 	cat TOTAL | tr '\n' '\t' >> $OUTFILE
 	echo >> $OUTFILE
-	echo -en "MySQL (localhost) \t$num_threads threads\tavg time\t" >> $OUTFILE
+	echo -en "MySQL (localhost) \t$num_threads threads\tavg resp time\t" >> $OUTFILE
 	cat AVG | tr '\n' '\t' >> $OUTFILE
+	echo >> $OUTFILE
+	echo -en "MySQL (localhost) \t$num_threads threads\ttransaction per sec\t" >> $OUTFILE
+	cat TRAN | tr '\n' '\t' >> $OUTFILE
 	echo >> $OUTFILE
 }
 
