8b7bd12daf6b03cc0d82ac2fcc9e9a0c1c612b4d
Shih-Wei Li <shihwei@cs.columbia.edu>
2015-03-12 11:22:35 -0400

    Added close file in show_result.py

--- show_result.py
+++ show_result.py
@@ -5,3 +5,4 @@ for line in file:
         components = line.strip().split("\t")
         sum = float(components[3]) + float(components[4]) + float(components[5])
         print sum / 3
+file.close()
